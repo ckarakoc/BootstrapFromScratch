5b6b9353acb7b9ce6665323586756f60d9c993e4
Celal Karakoç <c.karakoc@live.nl>
2025-07-07 20:35:35 +0200

    Refactored to scss. Added pricing-cards.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { PageNotFound } from './page-not-found/page-not-found';
+import { PricingCards } from './pricing-cards/pricing-cards';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: Home },
-  { path: '**', component: PageNotFound }
+  { path: 'pricing-cards', component: PricingCards },
+  { path: '**', component: PageNotFound },
 ];
---
+++ src/app/pricing-cards/pricing-cards.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing-cards',
+  imports: [],
+  templateUrl: './pricing-cards.html',
+  styleUrl: './pricing-cards.scss'
+})
+export class PricingCards {
+
+}
