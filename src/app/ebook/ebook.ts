20b6acae7b26119d1de0d071b99e5a35dc0dafa4
Celal Karakoç <c.karakoc@live.nl>
2025-07-11 22:21:40 +0200

    Added ebook initial part. Added hacky Angular Bootstrap way.

--- src/app/ebook/ebook.ts
+++ src/app/ebook/ebook.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faDollarSign, faRocket, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-ebook',
-  imports: [],
+  imports: [
+    RouterLink,
+    FaIconComponent,
+  ],
   templateUrl: './ebook.html',
   styleUrl: './ebook.scss'
 })
 export class Ebook {
+  protected readonly faUser = faUser;
+  protected readonly faRocket = faRocket;
+  protected readonly faDollarSign = faDollarSign;
 
+  scrolled = signal<boolean>(false);
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event): void {
+    this.scrolled.set(window.scrollY > 50);
+  }
 }
